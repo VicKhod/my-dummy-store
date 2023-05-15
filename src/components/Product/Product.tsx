723cf18f5aa39ebdf9f1372568536414926eda57
Виктория <vickhodulova@gmail.com>
2023-05-15 22:38:41 +0900

    feat: add prettier

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,28 +1,25 @@
-import React from 'react';
-import styles from './Product.module.scss';
+import { useEffect, useState } from "react";
+import styles from "./Product.module.scss";
+import { useGetProductQuery } from "../../services/api";
 
 export interface IProduct {
-  id: number,
-  title: string,
-  description: string,
-  price: number,
-  discountPercentage: number,
-  rating: number,
-  stock: number,
-  brand: string,
-  category: string,
-  thumbnail: string,
-  images: [ string ]
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: [string];
 }
 
 const Product = () => {
-  
+  const product = useGetProductQuery();
 
-  return (
-    <div className={styles.product}>
-      Product
-    </div>
-  )
+  return <div className={styles.product}>Product</div>;
 };
 
 export default Product;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,28 +1,26 @@
-import React from 'react';
-import styles from './HomePage.module.scss';
-import { useGetProductsQuery } from '../../services/api';
+import React from "react";
+import styles from "./HomePage.module.scss";
+import { useGetProductsQuery } from "../../services/api";
 
 interface HomePageProps {}
 
 const HomePage = ({}: HomePageProps) => {
-  const {data= [], isLoading} = useGetProductsQuery([]);
-  if (isLoading) return (<div>Loading...</div>)
-
-  const products = for (let item in data) {
-    return (<li key={item.id}>
-        {item.name}
-      </li>
-    )
-  }
-
+  const { data = [], isLoading } = useGetProductsQuery([]);
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <div className={styles.homePage}>
       <div>
-        
+        {Object.keys(data.products).map((key, index) => {
+          return (
+            <li key={index}>
+              {key}: {data[key]}
+            </li>
+          );
+        })}
       </div>
-  </div>
-  )
+    </div>
+  );
 };
 
 export default HomePage;
