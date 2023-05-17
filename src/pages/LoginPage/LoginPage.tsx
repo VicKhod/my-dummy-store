36333535858f32a6f3d7ef063247e87a928aadff
Виктория Илларионова <96482138+VicKhod@users.noreply.github.com>
2023-05-17 16:07:47 +0900

    fix: change LoginPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,11 +1,11 @@
-import {useLocation, useNavigate} from 'react-router-dom';
-import styles from './LoginPage.module.scss';
-import { useAppDispatch } from '../../components/hooks/store';
-import React from 'react';
-import { LoginRequest } from '../../store/slices/userSlice';
-import { useLoginMutation } from '../../services/api';
-import { setLogin } from '../../store/slices/authSlice';
-import { isRejectedWithValue } from '@reduxjs/toolkit';
+import { useLocation, useNavigate } from "react-router-dom";
+import styles from "./LoginPage.module.scss";
+import { useAppDispatch } from "../../components/hooks/store";
+import React from "react";
+import { LoginRequest } from "../../store/slices/userSlice";
+import { useLoginMutation } from "../../services/api";
+import { setLogin } from "../../store/slices/authSlice";
+import { isRejectedWithValue } from "@reduxjs/toolkit";
 
 interface LoginPageProps {
   onSubmit: (data: LoginRequest) => void;
@@ -16,26 +16,32 @@ type FormFields = {
   password: HTMLInputElement;
 };
 
-const LoginPage = ({onSubmit}: LoginPageProps) => {
+const LoginPage = ({ onSubmit }: LoginPageProps) => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const location = useLocation();
-  const fromPage = location.state?.from?.pathname || '/';
-  const [login, { isLoading }] = useLoginMutation();
+  const fromPage = location.state?.from?.pathname || "/";
+  const [login] = useLoginMutation();
   const [formState, setFormState] = React.useState<LoginRequest>({
-    username: '',
-    password: '',
+    username: "",
+    password: "",
   });
 
-  const handleSubmit: React.FormEventHandler<HTMLFormElement & FormFields> = (event) => {
+  const handleSubmit: React.FormEventHandler<HTMLFormElement & FormFields> = (
+    event
+  ) => {
     event.preventDefault();
     const form = event.currentTarget;
     const { username, password } = form;
-
     onSubmit({
       username: username.value,
       password: password.value,
-    })
+    });
+    setFormState(
+      { username: username.value, password: password.value }
+    );
+    console.log(formState)
+    dispatch(setLogin(formState));
   };
 
   return (
@@ -43,28 +49,35 @@ const LoginPage = ({onSubmit}: LoginPageProps) => {
       <form onSubmit={handleSubmit}>
         <label>
           <span>Username</span>
-          <input name="username" type="text" placeholder="Username" required/>
+          <input name="username" type="text" placeholder="Username" required />
         </label>
         <label>
           <span>Password</span>
-          <input name="password" type="password" placeholder="Password"  required/>
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            required
+          />
         </label>
       </form>
-      <button onClick={async () => {
-        try {
-          const user = await login(formState).unwrap()
-          dispatch(setLogin(user))
-          navigate(fromPage, {replace: true})
-        } catch (error: any) {
-          if (isRejectedWithValue(error)) {
-            console.log(error.data)
+      <button
+        onClick={async () => {
+          try {
+            const user = await login(formState).unwrap();
+            dispatch(setLogin(user));
+            navigate(fromPage, { replace: true });
+          } catch (error: any) {
+            if (isRejectedWithValue(error)) {
+              console.log(error.data);
+            }
           }
-        } 
-      }}>
-        Login   
+        }}
+      >
+        Login
       </button>
     </div>
-  )
+  );
 };
 
 export default LoginPage;
