7fa8e990830116818ae493935b9de3a741258e9d
Виктория <vickhodulova@gmail.com>
2024-02-05 21:28:29 +0900

    feat: change search btn icon

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,6 +1,6 @@
 import React, { HTMLInputTypeAttribute, InputHTMLAttributes, useState } from "react";
 import styles from "./Search.module.scss";
-import icon from "./img/search.svg";
+import icon from "../../assets/img/search.png";
 import { FormatInputPathObject } from "path";
 
 interface SearchProps {}
@@ -32,7 +32,7 @@ const Search = ({}: SearchProps) => {
       <form>
         <input type="text" placeholder="Поиск" ref={(input) => updateVal(input?.value)}/>
         <button className="search-btn" type="button">
-          <img src={icon} />
+          <img src={icon} alt="search"/>
         </button>
       </form>
     </div>
