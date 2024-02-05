8cffded65b2875faea8dd5134ceb7dcfbb4a7b8c
Виктория <vickhodulova@gmail.com>
2024-02-05 16:00:45 +0900

    feat: add img and text to bannet at HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./HomePage.module.scss";
+import bcgImg from "../../assets/img/background_home_page.png"
 
 // interface HomePageProps {}
 
@@ -7,7 +8,10 @@ const HomePage = () => {
   return (
     <>
       <div>
-        <img src="" alt="banner_image"/>
+        <img src={bcgImg} alt='banner_image'/>
+        <div>Offer products to make your interior Minimalist.</div>
+        <div>we have helped more than 1000+ people to give perfect comfort to the interior of their home</div>
+        <button>Find Product</button>
       </div>
       <div>
         <div>
