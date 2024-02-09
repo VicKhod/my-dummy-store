b3e344c516a31895d3365da4a08cea73d602462c
Виктория <vickhodulova@gmail.com>
2024-02-09 17:17:54 +0900

    feat: styling header links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 import Search from '../Search/Search';
 import iconCart from "../../assets/img/cart.png"
+import logo from "../../assets/img/logo.png"
 
 // interface HeaderProps {}
 
@@ -9,12 +10,18 @@ const Header = () => (
   <div className={styles.header}>
     <header>
       <nav>
-        <Link to='/'>Home</Link>
+        <div className={styles.header__nav_links}>
+        <img src={logo} alt="logo" />
+        </div>
+        <div className={styles.header__nav_links}>
+          <Link to='/'>Home</Link>
         <Link to='/catalog'>Catalog</Link>
         <Link to='/user'>Profile</Link>
-        <Search/>
+        </div>
+        <div className={styles.header__nav_links}>
+          <Search/>
         <Link to='/cart'><img src={iconCart} alt="cart" /></Link>
-        
+        </div>
       </nav>
 
     </header>
