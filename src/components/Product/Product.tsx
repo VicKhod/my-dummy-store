cf850af324bb4d39e1799c266a1c0e4d09dbcc18
Виктория <vickhodulova@gmail.com>
2024-02-16 16:35:49 +0900

    feat: add styles to Product

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,21 +1,24 @@
 import styles from "./Product.module.scss";
 import { IProduct } from "../../store/slices/productSlice";
 
-const Product: React.FC<{product: IProduct}> = ({product}) => {
+const Product: React.FC<{ product: IProduct }> = ({ product }) => {
   const productImg = {
     backgroundImage: `url(${product.images[0]})`,
     backgroundSize: "contain",
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
     backgroundColor: "#fff",
-    marginBottom: "20px"
-};
+    marginBottom: "20px",
+  };
 
   return (
     <div className={styles.productWrapper}>
       <div className={styles.productImg} style={productImg}></div>
-      <div className={styles.productTitle}>{product.title}</div>
-      <div className={styles.productPrice}>{product.price}</div>
+      <div className={styles.productDescription}>
+        <div className={styles.productCat}>{product.category}</div>
+        <div className={styles.productTitle}>{product.title}</div>
+        <div className={styles.productPrice}>${product.price}</div>
+      </div>
     </div>
   );
 };
