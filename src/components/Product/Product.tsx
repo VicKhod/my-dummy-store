94fc8977203b1b411ef15c092baf4b19496685ee
Виктория <vickhodulova@gmail.com>
2024-02-19 23:29:23 +0900

    feat: add button to Product

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,5 +1,6 @@
 import styles from "./Product.module.scss";
 import { IProduct } from "../../store/slices/productSlice";
+import addBtn from '../../assets/img/add-btn.png';
 
 const Product: React.FC<{ product: IProduct }> = ({ product }) => {
   const productImg = {
@@ -13,6 +14,7 @@ const Product: React.FC<{ product: IProduct }> = ({ product }) => {
         <div className={styles.productCat}>{product.category}</div>
         <div className={styles.productTitle}>{product.title}</div>
         <div className={styles.productPrice}>${product.price}</div>
+        <div className={styles.productChooseBtn}><img src={addBtn} alt="cart" /></div>
       </div>
     </div>
   );
